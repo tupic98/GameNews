5249d9196c97cb9981032519edf84cea3a1c2b9d
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-05 18:58:55 -0500

    Added functionality to login button. It actually logins

--- app/src/main/java/com/alvarenga/gamenews/LoginActivity.kt
+++ app/src/main/java/com/alvarenga/gamenews/LoginActivity.kt
@@ -1,27 +1,58 @@
 package com.alvarenga.gamenews
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.PersistableBundle
+import android.support.v4.app.FragmentManager
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.RelativeLayout
+import com.alvarenga.gamenews.gamenewsuca.api.APIRequest
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var relative1:RelativeLayout
-    var handler = Handler()
-    var runnable:Runnable = object:Runnable {
-        override fun run() {
-            relative1.visibility = View.VISIBLE
-        }
-    }
+    lateinit var usernametext:String
+    lateinit var passwordtext:String
+    lateinit var usernamefield:EditText
+    lateinit var passwordfield:EditText
+    lateinit var loginbutton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        relative1 = findViewById(R.id.RelLogoB)
+        settingViews()
 
-        handler.postDelayed(runnable, 2000)
+        loginbutton.setOnClickListener{ LoginButtonOperation(it)}
+
+        if(savedInstanceState != null){
+            usernamefield.apply {
+                (savedInstanceState.getString("username")) }
+            passwordfield.apply {
+                savedInstanceState.getString("password")
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState:Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("username",usernametext)
+        outState.putString("password",passwordtext)
+    }
+
+    private fun LoginButtonOperation(button:View){
+        var fragmentManager: FragmentManager = supportFragmentManager
+        APIRequest.login(usernamefield.text.toString(),passwordfield.text.toString(),this,fragmentManager)
+    }
+
+    private fun settingViews() {
+        usernamefield = findViewById(R.id.usernameedit)
+        usernametext = usernamefield.text.toString()
+        passwordfield = findViewById(R.id.passwordedit)
+        passwordtext = passwordfield.text.toString()
+        loginbutton = findViewById(R.id.loginbutton)
     }
 }
