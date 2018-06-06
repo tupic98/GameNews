6a7cd09157e23fd56032e27006848855d830a464
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-06 02:03:54 -0500

    Created de Dao for the Player entity

---
+++ app/src/main/java/com/alvarenga/gamenews/db/dao/PlayerDao.kt
@@ -0,0 +1,16 @@
+package com.alvarenga.gamenews.db.dao
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import com.alvarenga.gamenews.db.entity.PlayerEntity
+
+@Dao
+interface PlayerDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPlayer(player:PlayerEntity)
+    @Query("SELECT*FROM PlayerEntity WHERE game=:game")
+    fun getPlayer(game:String):LiveData<List<PlayerEntity>>
+}
