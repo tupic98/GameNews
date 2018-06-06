11cc9ef7b0069b6316af93748b57d4a24274b919
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-06 02:00:23 -0500

    Added singleton instance on db

---
+++ app/src/main/java/com/alvarenga/gamenews/db/AppDatabase.kt
@@ -0,0 +1,43 @@
+package com.alvarenga.gamenews.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import android.provider.CalendarContract
+import com.alvarenga.gamenews.db.dao.*
+import com.alvarenga.gamenews.db.entity.CategoryEntity
+import com.alvarenga.gamenews.db.entity.FavNewsEntity
+import com.alvarenga.gamenews.db.entity.NewsEntity
+import com.alvarenga.gamenews.db.entity.UserEntity
+
+@Database(
+        entities = [CategoryEntity::class,FavNewsEntity::class,NewsEntity::class,UserEntity::class],
+        exportSchema = false,
+        version = 1
+)
+abstract class AppDatabase:RoomDatabase(){
+
+    abstract fun categoryDao():CategoryDao
+    abstract fun favNewsDao():FavNewsDao
+    abstract fun newsDao():NewsDao
+    abstract fun UserDao():UserDao
+    abstract fun playerDao():PlayerDao
+
+    companion object {
+        private var INSTANCE:AppDatabase? = null
+        private val DBNAME:String = "gamenews.db"
+
+        fun getInstance(context: Context):AppDatabase?{
+            if(INSTANCE == null){
+                synchronized(AppDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                            AppDatabase::class.java, DBNAME)
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+
+}
