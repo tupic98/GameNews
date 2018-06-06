c20caa6b3faac84cb1b7649a94d42e76d975f253
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-05 19:05:58 -0500

    Added POST request to the api for login

--- app/src/main/java/com/alvarenga/gamenews/gamenewsuca/api/GameNewsucaAPI.kt
+++ app/src/main/java/com/alvarenga/gamenews/gamenewsuca/api/GameNewsucaAPI.kt
@@ -1,10 +1,19 @@
 package com.alvarenga.gamenews.gamenewsuca.api
 
+import com.alvarenga.gamenews.gamenewsuca.api.models.Login
 import retrofit2.Call
 import retrofit2.http.*
 
 interface GameNewsucaAPI {
     companion object {
-        val END_POINT = "http://gamenewsuca.herokuapp.com"
+        const val ENDPOINT = "http://gamenewsuca.herokuapp.com"
     }
+
+    @FormUrlEncoded
+    @POST("/login")
+    fun login(
+        @Field("user") username:String,
+        @Field("password") password:String
+    ): Call<Login>
+
 }
