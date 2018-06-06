b13c7f97a14e97c5de8e7659c7492fdfa306ac6d
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-06 01:59:16 -0500

    Added User model for the api

--- app/src/main/java/com/alvarenga/gamenews/gamenewsuca/api/models/User.kt
+++ app/src/main/java/com/alvarenga/gamenews/gamenewsuca/api/models/User.kt
@@ -2,7 +2,6 @@ package com.alvarenga.gamenews.gamenewsuca.api.models
 
 import com.google.gson.annotations.SerializedName
 
-
 class User{
 
     @SerializedName("_id")
@@ -10,5 +9,11 @@ class User{
 
     @SerializedName("created_date")
     private lateinit var date:String
+
+    @SerializedName("favoriteNews")
+    private  lateinit var favoriteNews:List<News>
+
+    private lateinit var user:String
+    private lateinit var password:String
 
 }
