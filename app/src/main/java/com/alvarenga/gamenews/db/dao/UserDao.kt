524cc7a96c54fbbee069a6db5ebdb7e7f6a2284f
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-13 17:52:11 -0500

    Added Replace onConflict

--- app/src/main/java/com/alvarenga/gamenews/db/dao/UserDao.kt
+++ app/src/main/java/com/alvarenga/gamenews/db/dao/UserDao.kt
@@ -2,12 +2,13 @@ package com.alvarenga.gamenews.db.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import com.alvarenga.gamenews.db.entity.UserEntity
 
 @Dao
 interface UserDao{
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertUser(user:UserEntity)
 
     @Query("SELECT*FROM UserEntity WHERE user like :username")
