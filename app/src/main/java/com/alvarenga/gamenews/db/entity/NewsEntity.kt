1948af8655bfd47a63f7a99c0c84daed13d7d3e7
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-13 19:20:23 -0500

    Added NonNull annotation to primary key field on News table

--- app/src/main/java/com/alvarenga/gamenews/db/entity/NewsEntity.kt
+++ app/src/main/java/com/alvarenga/gamenews/db/entity/NewsEntity.kt
@@ -3,13 +3,15 @@ package com.alvarenga.gamenews.db.entity
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.support.annotation.NonNull
 import java.io.FileDescriptor
 
 @Entity
-data class NewsEntity(@PrimaryKey @ColumnInfo(name = "_id") var id:String ="",
+data class NewsEntity(@NonNull @PrimaryKey @ColumnInfo(name = "_id") var id:String ="",
                       @ColumnInfo(name = "title") var title:String = "",
                       @ColumnInfo(name = "coverImage") var coverImage:String = "",
                       @ColumnInfo(name = "create_date") var createDate:String = "",
                       @ColumnInfo(name = "description") var description:String = "",
                       @ColumnInfo(name = "body") var body:String = "",
-                      @ColumnInfo(name = "game") var game:String = "")
+                      @ColumnInfo(name = "game") var game:String = "",
+                      @ColumnInfo(name = "favorites") var favorite:String = "")
