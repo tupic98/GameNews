a9a76e9cf837f0b4783ea00695c4383e365ec4f0
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-06-14 22:58:58 -0500

    Added functionality for newsfragment on the main activity

--- app/src/main/java/com/alvarenga/gamenews/MainActivity.kt
+++ app/src/main/java/com/alvarenga/gamenews/MainActivity.kt
@@ -1,34 +1,61 @@
 package com.alvarenga.gamenews
 
+import android.app.Activity
+import android.app.Application
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.alvarenga.gamenews.db.AppDatabase
+import com.alvarenga.gamenews.db.AppDatabase_Impl
+import com.alvarenga.gamenews.db.viewmodels.NewsViewModel
+import com.alvarenga.gamenews.extras.Utils.Companion.CheckNetConnection
+import com.alvarenga.gamenews.fragments.NewsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import org.jetbrains.anko.share
+import org.apache.commons.io.FileUtils
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    lateinit var appDB: AppDatabase
+    lateinit var newsVM:NewsViewModel
+    var FirstTimeApplicactionInitiated:Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        if(CheckLogin()){
+            setContentView(R.layout.activity_main)
+            if(!CheckNetConnection(this))
+                Toast.makeText(this, "No network connection", Toast.LENGTH_SHORT).show()
+            appDB = AppDatabase.getInstanceDatabase(applicationContext)!!
+            setContentView(R.layout.activity_main)
+            setSupportActionBar(toolbar)
+            val toggle = ActionBarDrawerToggle(
+                    this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+            drawer_layout.addDrawerListener(toggle)
+            toggle.syncState()
+            val fragment:Fragment = NewsFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.FrameLayout,fragment).commit()
+            supportActionBar!!.title = title
+            nav_view.setNavigationItemSelectedListener(this)
+        }else{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
-
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
@@ -40,15 +67,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
             R.id.action_settings -> return true
             else -> return super.onOptionsItemSelected(item)
@@ -56,29 +79,63 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
+        var fragmentTransaction = false
+        var fragment: Fragment? = null
+
         when (item.itemId) {
             R.id.nav_camera -> {
-                // Handle the camera action
+                fragment = NewsFragment()
+                fragmentTransaction = true
             }
             R.id.nav_gallery -> {
-
+                fragment = NewsFragment()
+                fragmentTransaction = true
             }
             R.id.nav_slideshow -> {
-
+                fragment = NewsFragment()
+                fragmentTransaction = true
             }
             R.id.nav_manage -> {
-
+                fragment = NewsFragment()
+                fragmentTransaction = true
             }
-            R.id.nav_share -> {
-
+            R.id.nav_settings -> {
+                fragment = NewsFragment()
+                fragmentTransaction = true
             }
-            R.id.nav_send -> {
-
+            R.id.nav_logout -> {
+                SignOut()
+                fragment = null
+                fragmentTransaction = false
             }
+
         }
-
+        if(fragmentTransaction){
+            supportFragmentManager.beginTransaction().replace(R.id.FrameLayout,fragment).commit()
+            supportActionBar!!.title = item.title
+            item.isChecked = true
+        }else{
+            item.isChecked = false
+        }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun CheckLogin():Boolean{
+        val sharedPref:SharedPreferences = this.getSharedPreferences("Login", Context.MODE_PRIVATE)
+        return sharedPref.contains("token")
+    }
+
+
+    private fun SignOut(){
+        val sharedPref = getSharedPreferences("Login", Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()){
+            clear()
+            apply()
+        }
+        newsVM.deleteAllNews()
+        startActivity(Intent(this, LoginActivity::class.java))
+        FileUtils.deleteQuietly(cacheDir)
+        finish()
     }
 }
